957e50609e63d5d1b4d790d5af4ac3cbee98d7c1
Ashley Turing <at@livetree.com>
2024-03-17 01:39:54 +0000

    Update GameManager.cs

--- Unity/Assets/Scripts/Managers/GameManager.cs
+++ Unity/Assets/Scripts/Managers/GameManager.cs
@@ -7,21 +7,17 @@ public class GameManager : MonoBehaviour
 {
     public void GoToLogin()
     {
-        Application.OpenURL("https://portal.livetree.com/login");
-    }
+         }
 
     public void GoToSignUp()
     {
-        Application.OpenURL("https://www.livetree.com/get-app");
-    }
+        }
 
     public void DownloadApp()
     {
-        Application.OpenURL("https://livetree.page.link/download");
-    }
+         }
 
     public void CreateContinue()
     {
-        SceneManager.LoadScene(1);
-    }
+        }
 }
